40c39721f97735b2fb8eeb1ddc63e3886dcddbc8
Joyal James <joyaljames@Joyals-MacBook-Air.local>
2025-04-12 23:41:40 +0100

    State Hook RECONCILATION

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,11 +1,21 @@
 import RestaurantCard from "./Restaurant";
 import { restList } from "../utils/constants";
+import { useState } from "react";
 const Body = ()=>{
+      // Local State Variable - Super powerful variable
+   const [listOfRestaurants, setListOfRestraunt] = useState(restList);
     return(
         <div>
-            <div className="search">Header</div>
+            <div className="search"><button onClick={() => {
+             const filteredList = listOfRestaurants.filter(
+               (res) => res.month > 5
+             );
+             setListOfRestraunt(filteredList);
+             console.log(filteredList);
+           }}>
+                Header</button></div>
             <div className="res-container">
-                {restList.map((restaurant)=> <RestaurantCard key={restaurant.id} resData={restaurant}/>)}
+                {listOfRestaurants.map((restaurant)=> <RestaurantCard key={restaurant.id} resData={restaurant}/>)}
             </div>
         </div>
     )
