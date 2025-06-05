0a99d02e7511a22753d1a164ce9dbc0e8fae0b14
Joyal James <joyaljames@Joyals-MacBook-Air.local>
2025-06-05 12:46:44 +0100

    Routing Link

--- src/App.js
+++ src/App.js
@@ -2,16 +2,45 @@ import React from "react";
 import ReactDOM from 'react-dom/client';
 import Header from "./components/Header";
 import Body from "./components/Body";
-
+import About from "./components/About";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import Contact from "./components/Contact";
 
 const AppLayout = ()=>{
     return(
         <div className="app">
             <Header/>
-            <Body/>
+            {/* Loading the pages according to the outlet   : Whenever there is a change in path the outlet will fill children accordingly to the path */}
+            <Outlet/>
         </div>
         
     )
 };
+
+const appRouter = createBrowserRouter([
+    {
+        path : "/",
+        element : <AppLayout/>,
+        // creating children route
+        children : [
+            {
+                path : "/",
+                element : <Body/> ,   //loads about component
+            },
+            {
+                path : "/about",
+                element : <About/> ,   //loads about component
+            },
+            {
+                path : "/contact",
+                element : <Contact/>
+            }
+        ],
+        errorElement : <Error/>,
+    },
+    
+])
+
 const root = ReactDOM.createRoot(document.getElementById("root")); 
-root.render(<AppLayout/>);
+
+root.render(<RouterProvider router={appRouter}/>);
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,17 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { LOGO_URL } from "../utils/constants";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   const[btnname,setBtnname] = useState("Login");
+
+  //if no dependency array => useEffect is called on every render
+  //if dependency array is empty = [] => useEffect is called on initial render(just once)
+  //if dependency array is [btnname] => called every time btnname is updated
+  useEffect(()=>{
+
+  },
+  [btnname])
   return(
   
     <div className="header">
@@ -14,9 +23,9 @@ const Header = () => {
           </div>
           <div className="nav-items">
             <ul>
-              <li>Home</li>
-              <li>About Us</li>
-              <li>Contact Us</li>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/about">About Us</Link></li>
+              <li><Link to="/contact">Contact Us</Link></li>
               <li>Cart</li>
               <button className="login" onClick={()=>{
                 btnname === "Login" ? setBtnname("Logout") : setBtnname("Login");
